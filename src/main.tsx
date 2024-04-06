7c05d684e1cca10d3b9f36fe5bb139501b35b693
Sniper <844489544@qq.com>
2024-04-06 18:51:30 +0800

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,28 @@
+import { StrictMode, Suspense } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  BrowserRouter,
+  useRoutes,
+} from 'react-router-dom'
+import './global.css';
+
+import routes from '~react-pages'
+
+function App() {
+  const element = useRoutes(routes)
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      {element}
+    </Suspense>
+  )
+}
+
+const app = createRoot(document.getElementById('root')!)
+
+app.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
