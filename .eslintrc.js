55a52fb26e7ed461bd8b2bdc40cf6fd639db4600
Sniper <844489544@qq.com>
2024-04-11 00:53:52 +0800

    feat: 优化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
   },
   settings: {
     react: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import Pages from 'vite-plugin-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    Pages(),
-  ],
+  plugins: [react(), Pages()],
 })
